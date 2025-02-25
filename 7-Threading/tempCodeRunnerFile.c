815375e12b66810a01f2d958ea8acfa67b2e907e
davidwong <david20030701@gmail.com>
2025-02-25 11:11:28 +0800

    compile files and modify some of them for better understanding

--- 7-Threading/tempCodeRunnerFile.c
+++ 7-Threading/tempCodeRunnerFile.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <unistd.h>
-#include <time.h>
 
 int shared = 50;
 
@@ -10,16 +8,13 @@ void *add(void *input)
 {
 
     int mylocal1 = shared;
-    printf("add thread:\tshared\t= %d\n", shared);
 
     mylocal1 += 10;
-    printf("add thread:\tmylocal1\t= %d\n", mylocal1);
 
     srand(time(NULL));
-    sleep(rand() % 3);
+    sleep(rand() % 2);
 
     shared = mylocal1;
-    printf("add thread write back:\tshared\t= %d\n", shared);
 
     pthread_exit(NULL);
 }
@@ -28,14 +23,11 @@ void *minus(void *input)
 {
 
     int mylocal2 = shared;
-    printf("minus thread:\tshared\t= %d\n", shared);
 
     mylocal2 -= 100;
-    printf("minus thread:\tmylocal2\t= %d\n", mylocal2);
 
     srand(time(NULL));
     sleep(rand() % 4);
-    printf("minus thread write back:\tshared\t= %d\n", shared);
 
     shared = mylocal2;
 
